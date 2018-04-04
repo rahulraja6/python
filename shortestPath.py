5a9c358b73bb6867ca1a63d715414c4d8c6b48bf
rahul <erahulraja@gmail.com>
2018-04-04 08:48:58 +0530

    practise2

--- shortestPath.py
+++ shortestPath.py
@@ -3,13 +3,9 @@ def shortestPath(longpath):
     ns=0
     ns=sum([ns+1 if i is 'N' else ns-1 if i is 'S' else ns+0 for i in longpath])
     we=sum([we+1 if i is 'W' else we-1 if i is 'E' else we+0 for i in longpath])
-    shortpath=''.join(['S'*abs(ns) if ns<0 else 'N'*abs(ns)])
-    shortpath=''.join([''.join(sorted([shortpath,'E'*abs(we)])) if we<0 else ''.join(sorted([shortpath,'W'*abs(we)]))])
-#    if we<0:
-#        shortpath=''.join(sorted([shortpath,'E'*abs(we)]))
-#    else:
-#        shortpath=''.join(sorted([shortpath,'W'*abs(we)]))
+#    shortpath='S'*abs(ns) if ns<0 else 'N'*abs(ns)
+    shortpath=''.join(sorted(['S'*abs(ns) if ns<0 else 'N'*abs(ns),'E'*abs(we)])) if we<0 else ''.join(sorted(['S'*abs(ns) if ns<0 else 'N'*abs(ns),'W'*abs(we)]))
     return shortpath
 
-print(shortestPath('WWWWNN'))
+print(shortestPath('NWESSSWWENNSW'))
     
