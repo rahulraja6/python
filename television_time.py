6a8e5d8e8f38e7ee0eb5065e5bba7b5167827054
rahul <erahulraja@gmail.com>
2018-04-08 12:16:56 +0530

    practice4

--- television_time.py
+++ television_time.py
@@ -1,7 +1,4 @@
-# -*- coding: utf-8 -*-
-
-intervals_list = [( 1, 4), (6, 8), (2, 4), (7, 9)]
-a=set([  [i for i in range(tup[0],tup[1],1)] for tup in intervals_list])
-#char=[[ch for ch in word] for word in ("apple", "banana", "pear", "the", "hello")]
-tup=( 1, 4)
-arr=[i for i in range(tup[0],tup[1],1)]
+intervals = [( 0, 4), (6, 8), (2, 4), (7, 9)]
+a=set()
+[ [a.add(i) for i in range(tup[0],tup[1])] for tup in intervals]
+print(a)
